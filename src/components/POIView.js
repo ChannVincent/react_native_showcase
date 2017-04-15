fb44b37537425d3b79b26690fe9239ab57e7dde8
Paul Billecocq <pb@smartapps.fr>
2017-04-15 21:09:31 +0200

    RN-6 : asset manager : load image from different path

--- src/components/POIView.js
+++ src/components/POIView.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { connect } from 'react-redux';
 import {  } from '../actions';
+
+import pathWallet from './pathWallet';
 
 class POIView extends Component {
   render() {
     return (
       <View style={ styles.containerStyle }>
           <Text>{ this.props.title }</Text>
+          <Image
+            style={{width: 100, height: 100}}
+            source={{ uri: pathWallet.lolcat }}
+          />
       </View>
     )
   }
---
+++ src/components/pathWallet.js
@@ -0,0 +1,5 @@
+const pathWallet = {
+    lolcat: 'file:///Users/pbillecocq/Documents/react_native_sandbox/react_native_showcase/assets/images/lolcat.jpg'
+  }
+
+export default pathWallet;

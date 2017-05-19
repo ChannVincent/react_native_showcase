1a4dcad73678300dea53999546ca82a21372d35a
Vincent Chann <vc@smartapps.fr>
2017-05-19 15:49:58 +0200

    RN-15 : audio player

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 import Slider from 'react-native-slider';
 import Video from 'react-native-video';
+const { width, height } = Dimensions.get('window');
 
 class AudioPlayer extends Component {
 
@@ -24,13 +25,20 @@ class AudioPlayer extends Component {
   }
 
   setTime(params) {
+    console.log('setTime - currentTime : ' + params.currentTime);
     if(!this.state.sliding) {
       this.setState({ currentTime: params.currentTime });
     }
   }
 
   onLoad(params) {
+    console.log('onLoad - duration : ' + params.duration);
     this.setState({ songDuration: params.duration });
+  }
+
+  onEnd() {
+    console.log('onEnd');
+    this.setState({ playing: false });
   }
 
   /*
@@ -50,10 +58,6 @@ class AudioPlayer extends Component {
     this.setState({ sliding: false });
   }
 
-  onEnd() {
-    this.setState({ playing: false });
-  }
-
   /*
   Life cycle
   */
@@ -67,14 +71,74 @@ class AudioPlayer extends Component {
       songPercentage = 0;
     }
 
-
     return (
       <View>
-
-        <Text>AudioPlayer</Text>
+        <Video
+          source={ require('../../assets/media63100.m4a') }
+          ref="audio"
+          volume={ this.state.muted ? 0 : 1.0 }
+          muted={ false }
+          paused={ !this.state.playing }
+          onLoad={ this.onLoad.bind(this) }
+          onProgress={ this.setTime.bind(this) }
+          onEnd={ this.onEnd.bind(this) }
+          resizeMode="cover"
+          repeat={ false }
+          playInBackground={ true }
+        />
+        <View style={ styles.sliderContainer }>
+          <Slider
+            onSlidingStart={ this.onSlidingStart.bind(this) }
+            onSlidingComplete={ this.onSlidingComplete.bind(this) }
+            onValueChange={ this.onSlidingChange.bind(this) }
+            minimumTrackTintColor='#851c44'
+            style={ styles.slider }
+            trackStyle={ styles.sliderTrack }
+            thumbStyle={ styles.sliderThumb }
+            value={ songPercentage }
+          />
+        </View>
       </View>
     )
   }
 }
 
+const styles = {
+  sliderContainer: {
+    margin: 5,
+    backgroundColor: '#eee',
+  },
+  timeInfo: {
+    flexDirection: 'row',
+  },
+  time: {
+    color: '#FFF',
+    flex: 1,
+    fontSize: 10,
+  },
+  timeRight: {
+    color: '#FFF',
+    textAlign: 'right',
+    flex: 1,
+    fontSize: 10,
+  },
+  slider: {
+    height: 20,
+  },
+  sliderTrack: {
+    height: 2,
+    backgroundColor: '#333',
+  },
+  sliderThumb: {
+    top: 11,
+    width: 14,
+    height: 14,
+    backgroundColor: '#f62976',
+    borderRadius: 7,
+    shadowColor: 'red',
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 2,
+    shadowOpacity: 1,
+  }
+}
 export default AudioPlayer;

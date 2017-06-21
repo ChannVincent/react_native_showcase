677d1b04fe959043dbaee41695c1f1bca43f518a
Vincent Chann <vc@smartapps.fr>
2017-06-21 10:23:29 +0200

    RN-18 : navigation : react-router-navigation

--- src/App.js
+++ src/App.js
@@ -6,14 +6,30 @@ import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import Router from './Router';
 
+import { BottomNavigation, Tab } from 'react-router-navigation';
+
 class App extends Component {
   render() {
     return (
-      <Provider store={ createStore(reducers, {}, applyMiddleware(ReduxThunk)) }>
-        <Router />
-      </Provider>
+      <BottomNavigation lazy={false} tabActiveTintColor="blue">
+          <Tab label="Feed" path="/feed" component={require('./components/ListPOI')} />
+          <Tab label="Profile" path="/profile" component={require('./components/KeypadPOI')} />
+      </BottomNavigation>
     )
   }
 }
 
 export default App;
+
+/*
+<NativeRouter>
+  <Navigation>
+    <BottomNavigation
+      lazy={false}
+      tabActiveTintColor="blue">
+        <Tab label="Feed" path="/KeypadPOI" component={require('./components/KeypadPOI')} />
+        <Tab label="Profile" path="/profile" component={require('./components/List')} />
+    </BottomNavigation>
+  </Navigation>
+</NativeRouter>
+*/
